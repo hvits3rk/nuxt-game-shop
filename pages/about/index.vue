68e60336388b79df731f85129a1423ad8a9d787f
Roman Tupikov <romkacg@gmail.com>
2018-03-08 22:56:32 +0300

    about page added

---
+++ pages/about/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="section">
+    <div class="content">
+      <div class="card">
+        <div class="card-content">
+          <p class="title">About Page</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam earum quaerat recusandae deserunt quas explicabo nesciunt, sit tempora nisi vitae, voluptatum laborum quasi sed! Temporibus vel ex reiciendis? Itaque, libero.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
