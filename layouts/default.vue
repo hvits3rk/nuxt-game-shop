601ee7b9cd667659c95deb4ffc723ad99c91634e
Roman Tupikov <romkacg@gmail.com>
2018-03-13 17:50:31 +0300

    Basic footer adde

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,15 +2,18 @@
   <div>
     <the-header/>
     <nuxt/>
+    <the-footer/>
   </div>
 </template>
 
 <script>
 import TheHeader from '@/components/navigation/TheHeader';
+import TheFooter from '@/components/navigation/TheFooter';
 
 export default {
   components: {
     TheHeader,
+    TheFooter,
   },
 };
 </script>
